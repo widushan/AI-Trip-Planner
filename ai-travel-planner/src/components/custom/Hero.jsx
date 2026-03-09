0eacb384039e788d1e9e92bd15d6d8c93f2f8c89
Pasindu Widushan <114930601+widushan@users.noreply.github.com>
2026-03-09 14:15:01 +0530

    placeAPI commit
    Integrating Places API

--- ai-travel-planner/src/components/custom/Hero.jsx
+++ ai-travel-planner/src/components/custom/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Button } from '../ui/button'
-
+import { Link } from 'react-router-dom'
 
 
 function Hero() {
@@ -12,7 +12,9 @@ function Hero() {
                 <span className='text-[#5c9df1]'>Discover Your Next Adventure with AI.</span><br /> Personalized Itineraries at Your Fingertips.
             </h1>
             <p className='text-center mt-5 text-xl text-gray-500'>Voygo leverages cutting-edge artificial intelligence to craft unique travel experiences tailored to your interests, budget, and style.</p>
-            <Button className='text-lg'>Get Started. It's Free</Button>
+            <Link to={"/create-trip"}>
+                <Button className='text-lg'>Get Started. It's Free</Button>
+            </Link>
         </div>
 
     )
--- ai-travel-planner/src/create-trip/index.jsx
+++ ai-travel-planner/src/create-trip/index.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete'
+
+
+
 
 function CreateTrip() {
+
     return (
-        <div>CreateTrip</div>
+
+        <div className='sm:px-10 md:px-32 lg:px-56 xl:px-72 px-5 mt-10'>
+            <h2 className='font-bold text-3xl'>Tell us your travel preferences</h2>
+            <p className='text-gray-500 mt-3 text-xl'>Tell us about your dream trip and we'll craft a personalized itinerary just for you.</p>
+
+            <div className='mt-20'>
+                <div>
+                    <h2 className='text-xl my-3 font-medium'>What is your destination?</h2>
+                    <GooglePlacesAutocomplete
+                        apiKey={import.meta.env.VITE_GOOGLE_PLACES_API_KEY}
+                    />
+                </div>
+            </div>
+        </div>
+
     )
+
 }
 
 export default CreateTrip
